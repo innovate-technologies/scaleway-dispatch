861d5b9f15f68620ba350b28d6babf925b81f99b
Maartje Eyskens <meyskens@me.com>
2017-09-11 11:36:00 +0200

    assign env endpoints

--- overlay/etc/profile.d/etcd.sh
+++ overlay/etc/profile.d/etcd.sh
@@ -1,2 +1,4 @@
 export ETCDCTL_CA_FILE=/etc/ssl/etcd/ca.pem
 export ETCDCTL_CACERT=/etc/ssl/etcd/ca.pem
+export ETCDCTL_ENDPOINT=https://localhost:2379
+export ETCDCTL_ENDPOINTS=https://localhost:2379
