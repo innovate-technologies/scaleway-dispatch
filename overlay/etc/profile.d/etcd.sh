9a6c95661db0e574a7e24dabef1d152762c64c16
Maartje Eyskens <maartje@eyskens.me>
2017-11-11 16:50:53 +0000

    fix var

--- overlay/etc/profile.d/etcd.sh
+++ overlay/etc/profile.d/etcd.sh
@@ -2,4 +2,4 @@ export ETCDCTL_CA_FILE=/etc/ssl/etcd/ca.pem
 export ETCDCTL_CACERT=/etc/ssl/etcd/ca.pem
 export ETCDCTL_ENDPOINT=https://localhost:2379
 export ETCDCTL_ENDPOINTS=https://localhost:2379
-export ETCDCTL_ENDPOINTS="$USER:$PASS"
+export ETCDCTL_USER="$USER:$PASS"
