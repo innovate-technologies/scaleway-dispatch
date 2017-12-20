9720584483e821a67b88a49d14c9128483bfb496
Maartje Eyskens <meyskens@me.com>
2017-12-20 09:51:35 +0100

    use etcdv3 in shell

--- overlay/etc/profile.d/etcd.sh
+++ overlay/etc/profile.d/etcd.sh
@@ -3,3 +3,4 @@ export ETCDCTL_CACERT=/etc/ssl/etcd/ca.pem
 export ETCDCTL_ENDPOINT=https://localhost:2379
 export ETCDCTL_ENDPOINTS=https://localhost:2379
 export ETCDCTL_USER="$USER:$PASS"
+export ETCDCTL_API=3
